dc69ca57fce7e9aaeec498afc6f2b87c205d3314
hsuhui <la10120818@yahoo.com.tw>
2021-08-04 15:25:00 +0800

    2021/08/04

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # tfjs-MNIST-demo
+
+## Use CDN 
+```javascript
+<!-- Import TensorFlow.js -->
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.8.0/dist/tf.min.js"></script>
+
+<!-- Import tfjs-vis -->
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis@1.0.2/dist/tfjs-vis.umd.min.js"></script>
+```
+
+## Load Data
+Each image is 28px wide 28px high and has a 1 color channel as it is a grayscale image. So the shape of each image is [28, 28, 1].
+<img src="images/Input_Data_Examples.png">
+
+
+## Train Model
+<img src="images/Model_Architecture.png">
+
+### There are 55,000 training materials and 10,000 verification materials.
+### When batch=1079, the accuracy very close to 1, and the loss=0.0136.
+<img src="images/Model_Training_onBatchEnd.png">
+
+### loss and val_loss are getting lower,finally loss=0.0644 and val_loss=0.0542.
+### acc and val_acc  are getting higher,in the end both are very close to 1.
+### Obviously,the model is good.
+<img src="images/Model_Training_onEpochEnd.png">
+
+## Make Predictions
+Take 500 images and predict what digit is in them.
+
+The argmax function provides the index of the highest probability category. 
+
+The model outputs the probability of each category. Here, find the highest probability and use it as a prediction
+
+## Confusion Matrix
+<img src="images/Confusion Matrix.png">
+
+## Evaluation
+### The average recognition accuracy rate is over __90%__
+<img src="images/Accuracy.png">
+
---
+++ scripts/script.js
@@ -0,0 +1,170 @@
+import {MnistData} from './data.js';
+
+const BATCH_SIZE = 512;
+const TRAIN_DATA_SIZE = 5500;
+const TEST_DATA_SIZE = 1000;
+async function showExamples(data) {
+    // Create a container in the visor
+    const surface =
+      tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data'});  
+  
+    // Get the examples
+    const examples = data.nextTestBatch(20);
+    const numExamples = examples.xs.shape[0];
+    
+    // Create a canvas element to render each example
+    for (let i = 0; i < numExamples; i++) {
+      const imageTensor = tf.tidy(() => {
+        // Reshape the image to 28x28 px
+        return examples.xs
+          .slice([i, 0], [1, examples.xs.shape[1]])
+          .reshape([28, 28, 1]);
+      });
+      
+      const canvas = document.createElement('canvas');
+      canvas.width = 28;
+      canvas.height = 28;
+      canvas.style = 'margin: 4px;';
+      await tf.browser.toPixels(imageTensor, canvas);
+      surface.drawArea.appendChild(canvas);
+  
+      imageTensor.dispose();
+    }
+  }
+  
+  async function run() {  
+    const data = new MnistData();
+    await data.load();
+    await showExamples(data);
+    const model = getModel();
+    tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
+  
+    await train(model, data);
+    await showAccuracy(model, data);
+    await showConfusion(model, data);
+    //await model.save('downloads://MNIST_Model');
+   
+  }
+  
+  document.addEventListener('DOMContentLoaded', run);
+
+function getModel() {
+  //使用序列模型
+  const model = tf.sequential();
+  
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const IMAGE_CHANNELS = 1;  
+  
+//在卷積神經網絡的第一層指定輸入形狀。 然後指定在這一層發生的卷積操作參數
+  model.add(tf.layers.conv2d({
+    inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+    kernelSize: 5,
+    filters: 8,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: 'varianceScaling'
+  }));
+
+  // 最大池化層充當一個區域的使用最大值的下採樣，而不是平均。
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+  
+  // 重複另一個卷積層+最大池化層。
+  // 注意在卷積中有更多的過濾器。
+  model.add(tf.layers.conv2d({
+    kernelSize: 5,
+    filters: 16,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: 'varianceScaling'
+  }));
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+  
+  //將 2D 過濾器的輸出扁平化為一維向量，用於輸入到最後一層。 這是一種準備高維數據到最終分類輸出層的常見做法。
+  model.add(tf.layers.flatten());
+
+  // 最後一層是一個密集層，它有 10 個輸出單元，每個單元一個類別。
+  // (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
+  const NUM_OUTPUT_CLASSES = 10;
+  model.add(tf.layers.dense({
+    units: NUM_OUTPUT_CLASSES,
+    kernelInitializer: 'varianceScaling',
+    activation: 'softmax'
+  }));
+
+  
+  // 選擇優化器、損失函數和準確度指標，然後編譯並返回模型
+  const optimizer = tf.train.adam();
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  return model;
+}
+//訓練模型
+async function train(model, data) {
+  const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+  const container = {
+    name: 'Model Training', tab: 'Model', styles: { height: '1000px' }
+  };
+  const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
+
+  const [trainXs, trainYs] = tf.tidy(() => {
+    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+    return [
+      d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
+      d.labels
+    ];
+  });
+
+  const [testXs, testYs] = tf.tidy(() => {
+    const d = data.nextTestBatch(TEST_DATA_SIZE);
+    return [
+      d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
+      d.labels
+    ];
+  });
+
+  return model.fit(trainXs, trainYs, {
+    batchSize: BATCH_SIZE,
+    validationData: [testXs, testYs],
+    epochs: 10,
+    shuffle: true,
+    callbacks: fitCallbacks
+  });
+}
+
+const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+function doPrediction(model, data, testDataSize = 500) {
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const testData = data.nextTestBatch(testDataSize);
+  const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+  const labels = testData.labels.argMax(-1);
+  const preds = model.predict(testxs).argMax(-1);
+
+  testxs.dispose();
+  return [preds, labels];
+}
+
+
+async function showAccuracy(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+  const container = {name: 'Accuracy', tab: 'Evaluation'};
+  tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+
+  labels.dispose();
+}
+
+async function showConfusion(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+  const container = {name: 'Confusion Matrix', tab: 'Evaluation'};
+  tfvis.render.confusionMatrix(container, {values: confusionMatrix, tickLabels: classNames});
+  labels.dispose();
+}
+
